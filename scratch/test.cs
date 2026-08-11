178c357754b3fc36689c6e34ff497b321a65e3f1
HieuBTFPT <buitrunghieucuber@gmail.com>
2026-08-11 15:45:34 +0700

    Update frontend build in backend wwwroot to fix FiSearch ReferenceError

--- scratch/test.cs
+++ scratch/test.cs
@@ -1,14 +1,17 @@
 using System;
-using System.Data.SqlClient;
+using System.IO;
+using System.Text.RegularExpressions;
 
 class Program {
     static void Main() {
-        string connStr = "workstation id=findfootball.mssql.somee.com;packet size=4096;user id=Lemonsumo982005_SQLLogin_1;pwd=3jyrpc9sgs;data source=findfootball.mssql.somee.com;persist security info=False;initial catalog=findfootball;TrustServerCertificate=True";
-        using(var conn = new SqlConnection(connStr)) {
-            conn.Open();
-            using(var cmd = new SqlCommand("UPDATE Users SET IsTwoFactorEnabled = 0, TwoFactorSecret = NULL;", conn)) {
-                int rows = cmd.ExecuteNonQuery();
-                Console.WriteLine("Updated " + rows + " users.");
+        var files = Directory.GetFiles("../FrontEnd/FrontendReact/src", "*.jsx", SearchOption.AllDirectories);
+        foreach(var file in files) {
+            var text = File.ReadAllText(file);
+            if (text.Contains("FiSearch")) {
+                bool hasImport = Regex.IsMatch(text, @"import\s+[^;]*FiSearch[^;]*\s+from", RegexOptions.Singleline);
+                if (!hasImport) {
+                    Console.WriteLine("Missing import in: " + file);
+                }
             }
         }
     }
